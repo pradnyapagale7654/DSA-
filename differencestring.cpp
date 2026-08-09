813ffbc4774a133f2a0bff9d6282d70725866e59
Pradnya Pagale <pradnyapagale7654@gmail.com>
2026-08-10 00:13:27 +0530

    difference between strings ascii method

---
+++ differencestring.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<unordered_map>
+using namespace std;
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int sums=0;
+        int sumt=0;
+        for(int i=0;i<s.length();i++){
+            sums+=s[i];
+        }
+        for(int j=0;j<t.length();j++){
+            sumt+=t[j];
+        }
+        return sumt-sums;
+    }
+};
+int main(){
+    Solution s;
+    string str1,str2;
+    cout<<"Enter the first string: ";
+    cin>>str1;
+    cout<<"Enter the second string: ";
+    cin>>str2;
+    char result=s.findTheDifference(str1,str2);
+    cout<<"The extra character in the second string is: "<<result<<endl;
+    return 0;
+}
