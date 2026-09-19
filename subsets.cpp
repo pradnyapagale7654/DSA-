02de90a7e9e575574bfd941328e695029dc6a879
Pradnya Pagale <pradnyapagale7654@gmail.com>
2026-09-19 22:52:57 +0530

    subsets

---
+++ subsets.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class solution{
+    public:
+void printsub(vector<int>&nums,vector<int>&ans,vector<vector<int>>&res,int i){
+if(nums.size()==i){
+    res.push_back(ans);
+    return ;
+}
+ans.push_back(nums[i]);   // first value include so push in ans 
+printsub(nums,ans,res,i+1);  // call for second value so i+1
+//backtracking
+ans.pop_back();
+printsub(nums,ans,res,i+1);
+}
+vector<vector<int>>subarray(vector<int>&nums){
+    vector<vector<int>>res;
+    vector<int>ans;
+    printsub(nums,ans,res,0);
+    return res;
+}
+};
+int main(){
+    solution s;
+    vector<int>nums={1,2,3};
+    vector<vector<int>>finalans=s.subarray(nums);
+    for (int i = 0; i < finalans.size(); i++) {
+    for (int j = 0; j < finalans[i].size(); j++) {
+        cout << finalans[i][j] << " ";
+    }
+    cout << endl;
+}
+    return 0;
+}
